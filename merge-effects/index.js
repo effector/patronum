680bff9e20404606ff82bdbbfc35b91a3beabe37
Egor <forcejmage@gmail.com>
2020-04-26 17:31:28 +0300

    feat(mergeeffects): add implementation without test and type

---
+++ merge-effects/index.js
@@ -0,0 +1,29 @@
+const { merge, combine } = require('effector');
+
+function mergeEffects(list) {
+  const done = merge(list.map((fx) => fx.done));
+  const fail = merge(list.map((fx) => fx.fail));
+  const doneData = merge(list.map((fx) => fx.doneData));
+  const failData = merge(list.map((fx) => fx.failData));
+  const pending = combine(
+    list.map((fx) => fx.pending),
+    (pendings) => pendings.some(Boolean),
+  );
+  const inFlight = combine(
+    list.map((fx) => fx.inFlight),
+    (counters) => counters.reduce((all, current) => all + current, 0),
+  );
+  const fxFinally = merge(list.map((fx) => fx.finally));
+
+  return {
+    done,
+    fail,
+    inFlight,
+    failData,
+    doneData,
+    pending,
+    finally: fxFinally,
+  };
+}
+
+module.exports = { mergeEffects };
---
+++ merge-effects/readme.md
@@ -0,0 +1,24 @@
+# Patronum/Reshape
+
+```ts
+import { mergeEffects } from 'patronum/merge-effects';
+```
+
+## Example
+
+```ts
+import { createEffect } from 'effector';
+import { mergeEffects } from 'patronum/merge-effects';
+
+const fx1 = createEffect({
+  handler: () => 1,
+});
+const fx2 = createEffect({
+  handler: () => 2,
+});
+
+mergeEffects([fx1, fx2]).done.watch(({ result }) => console.log(result));
+
+// => 1
+// => 2
+```
