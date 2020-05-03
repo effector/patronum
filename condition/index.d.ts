543701970096ec143bfe2e4603a111f60eede60c
Sergey Sova <5620073+sergeysova@users.noreply.github.com>
2020-05-03 16:49:41 +0300

    fix(condition): remove overload, support unit

--- condition/index.d.ts
+++ condition/index.d.ts
@@ -1,16 +1,8 @@
-import { Store, Event, Effect } from 'effector';
+import { Unit, Store } from 'effector';
 
-type Source<T> = Store<T> | Event<T> | Effect<T, any, any>;
-
-export function condition<State, Result extends State>(options: {
-  source: Source<State>;
-  if: (payload: State) => payload is Result;
-  then?: Source<Result>;
-  else?: Source<State>;
-});
 export function condition<State>(options: {
-  source: Source<State>;
+  source: Unit<State>;
   if: Store<boolean> | ((payload: State) => boolean) | State;
-  then?: Source<State>;
-  else?: Source<State>;
-});
+  then?: Unit<State>;
+  else?: Unit<State>;
+}): void;
