b2c023be6a74d82ad42d2b91383c7adb2bc15e69
doasync <doasync@gmail.com>
2020-06-06 00:59:53 +0300

    feat(combine-events): fix undefined payloads

---
+++ combineEvents/combine-events.test.js
@@ -0,0 +1,148 @@
+const { createEvent } = require('effector');
+const { combineEvents } = require('./index');
+
+const argumentHistory = (fn) => fn.mock.calls.map(([argument]) => argument);
+
+test('source: shape', () => {
+  const fn = jest.fn();
+
+  const event1 = createEvent();
+  const event2 = createEvent();
+  const event3 = createEvent();
+  const event4 = createEvent();
+  const event5 = createEvent();
+
+  const event = combineEvents({
+    event1,
+    event2,
+    event3,
+    event4,
+    event5,
+  });
+
+  event.watch(fn);
+
+  event1();
+  event1();
+  event2('-');
+  event3('c');
+  event2('b');
+  event2();
+  event4();
+  event4('d');
+  event5('e');
+
+  event1('a');
+  event2('-');
+  event3();
+  event2('b');
+  event3();
+  event4('-');
+  event4();
+  event5('e');
+
+  event1('1');
+  event2('-');
+  event3('-');
+  event2('2');
+  event3('3');
+  event4('-');
+  event4('4');
+  event5('5');
+  event5('-');
+
+  expect(argumentHistory(fn)).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "event1": undefined,
+        "event2": undefined,
+        "event3": "c",
+        "event4": "d",
+        "event5": "e",
+      },
+      Object {
+        "event1": "a",
+        "event2": "b",
+        "event3": undefined,
+        "event4": undefined,
+        "event5": "e",
+      },
+      Object {
+        "event1": "1",
+        "event2": "2",
+        "event3": "3",
+        "event4": "4",
+        "event5": "5",
+      },
+    ]
+  `);
+});
+
+test('source: array', () => {
+  const fn = jest.fn();
+
+  const event1 = createEvent();
+  const event2 = createEvent();
+  const event3 = createEvent();
+  const event4 = createEvent();
+  const event5 = createEvent();
+
+  const event = combineEvents([event1, event2, event3, event4, event5]);
+
+  event.watch(fn);
+
+  event1();
+  event1();
+  event2('-');
+  event3('c');
+  event2('b');
+  event2();
+  event4();
+  event4('d');
+  event5('e');
+
+  event1('a');
+  event2('-');
+  event3();
+  event2('b');
+  event3();
+  event4('-');
+  event4();
+  event5('e');
+
+  event1('1');
+  event2('-');
+  event3('-');
+  event2('2');
+  event3('3');
+  event4('-');
+  event4('4');
+  event5('5');
+  event5('-');
+
+  expect(argumentHistory(fn)).toMatchInlineSnapshot(`
+    Array [
+      Array [
+        undefined,
+        undefined,
+        "c",
+        "d",
+        "e",
+      ],
+      Array [
+        "a",
+        "b",
+        undefined,
+        undefined,
+        "e",
+      ],
+      Array [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+      ],
+    ]
+  `);
+});
--- combineEvents/index.js
+++ combineEvents/index.js
@@ -1,28 +1,26 @@
-const {
-  createStore,
-  createEvent,
-  guard,
-  restore,
-  combine,
-  sample,
-} = require('effector');
+const { createStore, createEvent, guard, merge, sample } = require('effector');
 
 function combineEvents(events) {
   const target = createEvent();
-  const shape = Array.isArray(events) ? [] : {};
+  const isArray = Array.isArray(events);
   const keys = Object.keys(events);
   const counter = createStore(keys.length).reset(sample(target));
+  const results = createStore(isArray ? [] : {}).reset(target);
 
   for (const key of keys) {
     const done = createStore(false)
       .on(events[key], () => true)
       .reset(target);
-    shape[key] = restore(events[key], null).reset(target);
     counter.on(done, (value) => value - 1);
+    results.on(events[key], (shape, payload) => {
+      // eslint-disable-next-line no-param-reassign
+      shape[key] = payload;
+      return isArray ? [...shape] : { ...shape };
+    });
   }
 
   guard({
-    source: combine(shape),
+    source: sample(results, merge(Object.values(events))),
     filter: counter.map((value) => value === 0),
     target,
   });
