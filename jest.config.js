4dd6dc4a39db1813a6c8223a3a2f65e31fdc3f62
doasync <doasync@gmail.com>
2020-06-07 09:04:35 +0300

    TypeScript support for Jest (#12)
    * test(config): support tests in typescript
    
    * test(config): do not process jsx/tsx files
    
    * test(config): fix different dev environments

--- jest.config.js
+++ jest.config.js
@@ -168,7 +168,9 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   // transform: undefined,
-
+  transform: {
+    '^.+\\.[t|j]s$': 'babel-jest',
+  },
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
   //   "/node_modules/"
