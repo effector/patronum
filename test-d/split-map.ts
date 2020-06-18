fcb2db523cf8bc2c76c5ca606e4e03492545f6da
Sergey Sova <s.sova@redmadrobot.com>
2020-06-18 14:55:19 +0300

    test(split-map): add type tests
    disabled because tsd means that objects is different
    
    re #22

---
+++ test-d/split-map.ts
@@ -0,0 +1,60 @@
+// import { expectType, expectError } from 'tsd';
+// import { Store, Event, createStore, createEvent, createEffect } from 'effector';
+// import { splitMap } from '../split-map';
+
+// // Allow any unit as source
+// {
+//   const event = createEvent<number>();
+//   const $store = createStore(0);
+//   const effect = createEffect<string, void>();
+
+//   expectType<{ demo: Event<boolean>; __: Event<number> }>(
+//     splitMap({
+//       source: event,
+//       cases: {
+//         demo: () => true,
+//       },
+//     }),
+//   );
+//   expectType<{ demo: Event<boolean>; __: Event<number> }>(
+//     splitMap({
+//       source: $store,
+//       cases: {
+//         demo: () => true,
+//       },
+//     }),
+//   );
+//   expectType<{ demo: Event<boolean>; __: Event<string> }>(
+//     splitMap({
+//       source: effect,
+//       cases: {
+//         demo: () => true,
+//       },
+//     }),
+//   );
+// }
+
+// // Has default case
+// {
+//   expectType<{ __: Event<number> }>(
+//     splitMap({ source: createEvent<number>(), cases: {} }),
+//   );
+//   expectType<{ __: Event<{ demo: number }> }>(
+//     splitMap({ source: createEvent<{ demo: number }>(), cases: {} }),
+//   );
+// }
+
+// // Omit undefined from object type
+// {
+//   type Payload = { key?: string };
+//   const source = createEvent<Payload>();
+
+//   expectType<{ example: Event<string>; __: Event<Payload> }>(
+//     splitMap({
+//       source,
+//       cases: {
+//         example: (object) => object.key,
+//       },
+//     }),
+//   );
+// }
