e86af637d1bbb3d6e1caa674a41c321df553dc92
doasync <doasync@gmail.com>
2020-06-30 09:26:56 +0300

    feat(combine-events): enhanced types

--- combine-events/index.d.ts
+++ combine-events/index.d.ts
@@ -1,17 +1,35 @@
-/* eslint-disable prettier/prettier */
-import { Unit, Event } from 'effector';
+import { Unit, Store, Event, Effect } from 'effector';
 
 type Tuple<T = unknown> = [T] | T[];
-type Shape = { [key: string]: Event<any> } | Tuple<Event<any>>;
+type Results = { [key: string]: any } | Tuple<any>;
+type Events<Result> = {
+  [Key in keyof Result]: Event<Result[Key]>;
+};
 
-export function combineEvents<Events extends Shape, Target>(_: {
-  events: Events;
+type ReturnTarget<Result, Target> = Target extends Store<infer S>
+  ? S extends Result
+    ? Store<S>
+    : Store<Result>
+  : Target extends Event<infer E>
+  ? E extends Result
+    ? Event<E>
+    : Event<Result>
+  : Target extends Effect<infer P, infer D, infer F>
+  ? P extends Result
+    ? Effect<P, D, F>
+    : Effect<Result, D, F>
+  : Unit<Result>;
+
+export function combineEvents<T extends Results>(config: {
+  events: Events<T>;
   reset?: Unit<any>;
-  target?: Target;
-}): Target extends Unit<infer T>
-  ? Target
-  : Event<
-      {
-        [K in keyof Events]: Events[K] extends Event<infer U> ? U : never;
-      }
-    >;
+}): Event<T>;
+
+export function combineEvents<
+  T extends Results,
+  Target extends Unit<Results>
+>(config: {
+  events: Events<T>;
+  target: Target;
+  reset?: Unit<any>;
+}): ReturnTarget<T, Target>;
