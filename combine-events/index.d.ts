ba1c76d4d9508d3d475f7938e63e2629d4a4b0a5
Sergey Sova <s.sova@redmadrobot.com>
2020-07-07 19:21:07 +0300

    fix(combine-events): do not use enhanced typings

--- combine-events/index.d.ts
+++ combine-events/index.d.ts
@@ -1,35 +1,14 @@
-import { Unit, Store, Event, Effect } from 'effector';
+import { Event } from 'effector';
 
 type Tuple<T = unknown> = [T] | T[];
-type Results = { [key: string]: any } | Tuple<any>;
-type Events<Result> = {
-  [Key in keyof Result]: Event<Result[Key]>;
-};
 
-type ReturnTarget<Result, Target> = Target extends Store<infer S>
-  ? S extends Result
-    ? Store<S>
-    : Store<Result>
-  : Target extends Event<infer E>
-  ? E extends Result
-    ? Event<E>
-    : Event<Result>
-  : Target extends Effect<infer P, infer D, infer F>
-  ? P extends Result
-    ? Effect<P, D, F>
-    : Effect<Result, D, F>
-  : Unit<Result>;
-
-export function combineEvents<T extends Results>(config: {
-  events: Events<T>;
-  reset?: Unit<any>;
-}): Event<T>;
-
-export function combineEvents<
-  T extends Results,
-  Target extends Unit<Results>
->(config: {
-  events: Events<T>;
-  target: Target;
-  reset?: Unit<any>;
-}): ReturnTarget<T, Target>;
+export function combineEvents<Shape extends Tuple>(
+  shape: Shape,
+): Event<
+  { [K in keyof Shape]: Shape[K] extends Event<infer U> ? U : Shape[K] }
+>;
+export function combineEvents<Shape>(
+  shape: Shape,
+): Event<
+  { [K in keyof Shape]: Shape[K] extends Event<infer U> ? U : Shape[K] }
+>;
