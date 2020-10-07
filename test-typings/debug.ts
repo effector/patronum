a42de3d4e7ed8d1711484a9083a6aa6c082710c2
Sergey Sova <mail@sergeysova.com>
2020-10-07 09:51:26 +0300

    test(debug): cover with type tests
    re #22

---
+++ test-typings/debug.ts
@@ -0,0 +1,25 @@
+import { expectType } from 'tsd';
+import { createEvent, createDomain, createEffect, createStore } from 'effector';
+import { debug } from '../debug';
+
+// Allows each unit of effector
+{
+  const event = createEvent<number>();
+  const $store = createStore('');
+  const fx = createEffect<boolean, void, number>();
+  const domain = createDomain();
+  expectType<void>(debug(event, $store, fx, domain));
+}
+
+// Allows single argument
+{
+  const event = createEvent<number>();
+  const $store = createStore('');
+  const fx = createEffect<boolean, void, number>();
+  const domain = createDomain();
+
+  debug(event);
+  debug($store);
+  debug(fx);
+  debug(domain);
+}
