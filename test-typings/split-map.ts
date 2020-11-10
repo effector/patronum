cabfbb750b370def3a6b508e793e7497319e8d06
Sergey Sova <mail@sergeysova.com>
2020-11-10 11:47:07 +0300

    test(split-map): add type tests
    closes #71

--- test-typings/split-map.ts
+++ test-typings/split-map.ts
@@ -1,60 +1,96 @@
-// import { expectType } from 'tsd';
-// import { Store, Event, createStore, createEvent, createEffect } from 'effector';
-// import { splitMap } from '../split-map';
+import { expectType } from 'tsd';
+import {
+  Store,
+  Event,
+  createStore,
+  createEvent,
+  createEffect,
+  sample,
+} from 'effector';
+import { splitMap } from '../split-map';
 
-// // Allow any unit as source
-// {
-//   const event = createEvent<number>();
-//   const $store = createStore(0);
-//   const effect = createEffect<string, void>();
+// partial destructuring throws an error #71
+{
+  const $settings = createStore({
+    MIN_LVL: 42,
+  });
 
-//   expectType<{ demo: Event<boolean>; __: Event<number> }>(
-//     splitMap({
-//       source: event,
-//       cases: {
-//         demo: () => true,
-//       },
-//     }),
-//   );
-//   expectType<{ demo: Event<boolean>; __: Event<number> }>(
-//     splitMap({
-//       source: $store,
-//       cases: {
-//         demo: () => true,
-//       },
-//     }),
-//   );
-//   expectType<{ demo: Event<boolean>; __: Event<string> }>(
-//     splitMap({
-//       source: effect,
-//       cases: {
-//         demo: () => true,
-//       },
-//     }),
-//   );
-// }
+  const someEvt = createEvent<{ lvl: number }>();
 
-// // Has default case
-// {
-//   expectType<{ __: Event<number> }>(
-//     splitMap({ source: createEvent<number>(), cases: {} }),
-//   );
-//   expectType<{ __: Event<{ demo: number }> }>(
-//     splitMap({ source: createEvent<{ demo: number }>(), cases: {} }),
-//   );
-// }
+  const happened = sample({
+    source: $settings,
+    clock: someEvt,
+    fn: (settings, payload) => ({ settings, payload }),
+  });
 
-// // Omit undefined from object type
-// {
-//   type Payload = { key?: string };
-//   const source = createEvent<Payload>();
+  const { highLvl, lowLvl } = splitMap({
+    source: happened,
+    cases: {
+      highLvl: ({ payload }) => (payload.lvl >= 50 ? payload.lvl : undefined),
+      lowLvl: ({ settings, payload }) =>
+        payload.lvl < settings.MIN_LVL ? payload.lvl : undefined,
+    },
+  });
 
-//   expectType<{ example: Event<string>; __: Event<Payload> }>(
-//     splitMap({
-//       source,
-//       cases: {
-//         example: (object) => object.key,
-//       },
-//     }),
-//   );
-// }
+  expectType<Event<number>>(highLvl);
+  expectType<Event<number>>(lowLvl);
+}
+
+// Allow any unit as source
+{
+  const event = createEvent<number>();
+  const $store = createStore(0);
+  const effect = createEffect<string, void>();
+
+  expectType<{ demo: Event<boolean>; __: Event<number> }>(
+    splitMap({
+      source: event,
+      cases: {
+        demo: () => true,
+      },
+    }),
+  );
+  expectType<{ demo: Event<boolean>; __: Event<number> }>(
+    splitMap({
+      source: $store,
+      cases: {
+        demo: () => true,
+      },
+    }),
+  );
+  expectType<{ demo: Event<boolean>; __: Event<string> }>(
+    splitMap({
+      source: effect,
+      cases: {
+        demo: () => true,
+      },
+    }),
+  );
+}
+
+// Has default case
+{
+  expectType<{ __: Event<number> }>(
+    splitMap({ source: createEvent<number>(), cases: {} }),
+  );
+  expectType<{ __: Event<{ demo: number }> }>(
+    splitMap({ source: createEvent<{ demo: number }>(), cases: {} }),
+  );
+}
+
+// Omit undefined from object type
+{
+  interface Payload {
+    key?: string;
+  }
+  const source = createEvent<Payload>();
+
+  expectType<{ example: Event<string>; __: Event<Payload> }>(
+    splitMap({
+      source,
+      cases: {
+        example: (object) => object.key,
+      },
+    }),
+  );
+}
