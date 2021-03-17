f5421e91a7dae2018eade29c9eb6fa75919a0abc
Sergey Sova <sergeysova@users.noreply.github.com>
2021-03-17 15:52:38 +0300

    test: add sid snapshots

--- integration/cra/src/demo.test.js
+++ integration/cra/src/demo.test.js
@@ -3,5 +3,5 @@ import { $pending } from './demo';
 test('should have sid', () => {
   expect($pending.sid).toBeDefined();
   expect($pending.sid).not.toBeNull();
-  expect($pending.sid).toMatchInlineSnapshot();
+  expect($pending.sid).toMatchInlineSnapshot(`"-1ka365ɔpending"`);
 });
