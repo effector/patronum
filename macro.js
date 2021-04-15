b0de12fae2fbb1f2207f6a51de9f54d7dcfebb3f
Sergey Sova <mail@sergeysova.com>
2021-04-15 10:33:53 +0300

    feat: remove babel traverse and helper from deps

--- macro.js
+++ macro.js
@@ -1,7 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { createMacro } = require('babel-plugin-macros');
-const { addNamed } = require('@babel/helper-module-imports');
-const { default: traverse } = require('@babel/traverse');
 const babelPlugin = require('effector/babel-plugin');
 const { factories, mapping } = require('./babel-plugin-factories.json');
 
@@ -21,12 +19,10 @@ function patronum({
     const methodImportPath = importFromRoot
       ? importModuleName
       : importModuleName + '/' + mapping[referenceName];
-    const id = addNamed(program, referenceName, methodImportPath, {
-      nameHint: referenceName,
-    });
+    const id = addImport(babel.types, program, referenceName, methodImportPath);
 
     references[referenceName].forEach((referencePath) => {
-      referencePath.node.name = id.name;
+      referencePath.node.name = id;
     });
   });
 
@@ -38,9 +34,35 @@ function patronum({
   });
 
   instance.pre();
-  traverse(program.parent, instance.visitor, undefined, {
+  babel.traverse(program.parent, instance.visitor, undefined, {
     ...state,
     ...instance,
   });
   instance.post();
+}
+
+function addImport(t, programPath, specifierName, importPath) {
+  const [newPath] = programPath.unshiftContainer(
+    'body',
+    t.importDeclaration(
+      [
+        t.importSpecifier(
+          programPath.scope.generateUidIdentifier(specifierName),
+          t.identifier(specifierName),
+        ),
+      ],
+      t.stringLiteral(importPath),
+    ),
+  );
+
+  let found;
+
+  newPath.get('specifiers').forEach((specifier) => {
+    if (specifier.node.imported.name === specifierName) {
+      found = specifier;
+    }
+  });
+
+  programPath.scope.registerBinding('module', found);
+  return found.node.local.name;
 }
