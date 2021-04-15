07d96ecc2e255e03ad0ecd7f750a5e7235b61eda
Sergey Sova <mail@sergeysova.com>
2021-04-15 11:07:18 +0300

    fix: jest should not compile js files with babel

--- jest.config.js
+++ jest.config.js
@@ -155,7 +155,7 @@ module.exports = {
   // A map from regular expressions to paths to transformers
   // transform: undefined,
   transform: {
-    '^.+\\.[t|j]s$': 'babel-jest',
+    '^.+\\.ts$': 'babel-jest',
   },
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
