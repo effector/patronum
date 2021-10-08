398eb6dd4da35fff4d4ac907289109ea79c32889
Sergey Sova <mail@sergeysova.com>
2021-10-08 03:37:21 +0300

    refactor(pending): to ts

--- src/pending/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Store, Effect, Domain } from 'effector';
-
-export type Strategy = 'some' | 'every';
-
-export function pending(config: {
-  effects: Array<Effect<any, any, any>>;
-  of?: Strategy;
-}): Store<boolean>;
-export function pending(config: { domain: Domain; of?: Strategy }): Store<boolean>;
