dec83832ecb5dcd177fecca730a8001ebfd79df2
Sergey Sova <mail@sergeysova.com>
2021-10-08 04:02:28 +0300

    refactor(spread): to ts

---
+++ src/spread/index.ts
@@ -0,0 +1,55 @@
+import { createEvent, Event, guard, sample, Unit } from 'effector';
+
+type NoInfer<T> = [T][T extends any ? 0 : never];
+type EventAsReturnType<Payload> = any extends Payload ? Event<Payload> : never;
+
+export function spread<Payload>(config: {
+  targets: {
+    [Key in keyof Payload]?: Unit<Payload[Key]>;
+  };
+}): EventAsReturnType<Payload>;
+
+export function spread<
+  Source,
+  Payload extends Source extends Unit<infer S> ? S : never,
+>(config: {
+  source: Source;
+  targets: {
+    [Key in keyof Payload]?: Unit<NoInfer<Payload[Key]>>;
+  };
+}): Source;
+
+/**
+ * @example
+ * spread({ source: dataObject, targets: { first: targetA, second: targetB } })
+ * forward({
+ *   to: spread({targets: { first: targetA, second: targetB } })
+ * })
+ */
+export function spread<P>({
+  targets,
+  source = createEvent<P>(),
+}: {
+  targets: {
+    [Key in keyof P]?: Unit<P[Key]>;
+  };
+  source?: Unit<P>;
+}): EventAsReturnType<P> {
+  for (const targetKey in targets) {
+    if (targetKey in targets) {
+      const hasTargetKey = guard({
+        source,
+        filter: (object) =>
+          typeof object === 'object' && object !== null && targetKey in object,
+      });
+
+      sample({
+        source: hasTargetKey,
+        fn: (object) => object[targetKey],
+        target: targets[targetKey] as any,
+      });
+    }
+  }
+
+  return source as any;
+}
