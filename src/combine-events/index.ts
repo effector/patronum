f8d4ebd13e91742b5ba648aefd90cc4dc9589d3c
Sergey Sova <mail@sergeysova.com>
2021-10-08 03:03:26 +0300

    refactor(combine-events): to ts

---
+++ src/combine-events/index.ts
@@ -0,0 +1,102 @@
+import {
+  createEvent,
+  createStore,
+  Effect,
+  Event,
+  guard,
+  is,
+  merge,
+  sample,
+  Store,
+  Unit,
+  withRegion,
+} from 'effector';
+
+type Tuple<T = unknown> = [T] | T[];
+type Shape = Record<string, unknown> | Tuple;
+
+type Events<Result> = {
+  [Key in keyof Result]: Event<Result[Key]>;
+};
+
+type ReturnTarget<Result, Target> = Target extends Store<infer S>
+  ? S extends Result
+    ? Store<S>
+    : Store<Result>
+  : Target extends Event<infer P>
+  ? P extends Result
+    ? Event<P>
+    : Event<Result>
+  : Target extends Effect<infer P, infer D, infer F>
+  ? P extends Result
+    ? Effect<P, D, F>
+    : Effect<Result, D, F>
+  : Unit<Result>;
+
+export function combineEvents<P extends Shape>(config: {
+  events: Events<P>;
+  reset?: Unit<any>;
+}): Event<P>;
+
+export function combineEvents<
+  P extends Shape,
+  T extends Unit<P extends Tuple ? P : Partial<P>>,
+>(config: { events: Events<P>; target: T; reset?: Unit<any> }): ReturnTarget<P, T>;
+
+export function combineEvents<P>({
+  events,
+  reset,
+  target = createEvent(),
+}: {
+  events: Events<P>;
+  reset?: Unit<any>;
+  target?: Unit<any>;
+}) {
+  if (!is.unit(target)) throwError('target should be a unit');
+  if (reset && !is.unit(reset)) throwError('reset should be a unit');
+
+  withRegion(target, () => {
+    const keys = Object.keys(events);
+    const defaultShape = Array.isArray(events) ? [...keys].fill('') : {};
+
+    const $counter = createStore(keys.length);
+    const $results = createStore(defaultShape);
+
+    $counter.reset(sample(target));
+    $results.reset(target);
+
+    if (reset) {
+      $counter.reset(sample(reset));
+      $results.reset(reset);
+    }
+
+    for (const key of keys) {
+      const $isDone = createStore(false)
+        .on(events[key], () => true)
+        .reset(target);
+
+      if (reset) {
+        $isDone.reset(reset);
+      }
+
+      $counter.on($isDone, (value) => value - 1);
+      $results.on(events[key], (shape, payload) => {
+        const newShape = Array.isArray(shape) ? [...shape] : { ...shape };
+        newShape[key] = payload;
+        return newShape;
+      });
+    }
+
+    guard({
+      source: sample($results, merge(Object.values(events))),
+      filter: $counter.map((value) => value === 0),
+      target,
+    });
+  });
+
+  return target;
+}
+
+function throwError(message: string) {
+  throw new Error(message);
+}
