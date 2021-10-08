9f81600ff98e46245ae22fe08df0d278547eda10
Sergey Sova <mail@sergeysova.com>
2021-10-08 03:29:23 +0300

    refactor(in-flight): to ts

--- src/in-flight/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Store, Effect, Domain } from 'effector';
-
-export function inFlight(_: { effects: Array<Effect<any, any, any>> }): Store<number>;
-
-export function inFlight(_: { domain: Domain }): Store<number>;
