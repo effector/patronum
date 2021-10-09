2924254355a14cce6a0899f6855ab28abde8a5d1
Sergey Sova <mail@sergeysova.com>
2021-10-10 00:32:13 +0300

    test(time): add tests for the new method

---
+++ src/time/time.fork.test.ts
@@ -0,0 +1,53 @@
+import 'regenerator-runtime/runtime';
+import { createEvent, fork, serialize, allSettled } from 'effector';
+
+import { time } from './index';
+
+test('time with custom timing reading fn should not affect different scopes', async () => {
+  let counter = 0;
+  const clock = createEvent();
+  const $time = time({ clock, getNow: () => ++counter });
+
+  const scopeA = fork();
+  const scopeB = fork();
+
+  expect(scopeA.getState($time)).toBe(1);
+  expect(scopeB.getState($time)).toBe(1);
+
+  await allSettled(clock, { scope: scopeA });
+  expect(scopeA.getState($time)).toBe(2); // changed only scopeA
+  expect(scopeB.getState($time)).toBe(1);
+
+  await allSettled(clock, { scope: scopeA });
+  expect(scopeA.getState($time)).toBe(3); // changed only scopeA
+  expect(scopeB.getState($time)).toBe(1);
+
+  await allSettled(clock, { scope: scopeB });
+  expect(scopeA.getState($time)).toBe(3);
+  expect(scopeB.getState($time)).toBe(4); // changed only scopeB with the gap
+});
+
+test('store must correctly serializes', async () => {
+  let counter = 0;
+  const clock = createEvent();
+  const $time = time({ clock, getNow: () => ++counter });
+
+  const scopeA = fork();
+  const scopeB = fork();
+
+  expect(serialize(scopeA)).toMatchInlineSnapshot(`Object {}`);
+  expect(serialize(scopeB)).toMatchInlineSnapshot(`Object {}`);
+
+  await allSettled(clock, { scope: scopeA });
+  await allSettled(clock, { scope: scopeB });
+  expect(serialize(scopeA)).toMatchInlineSnapshot(`
+    Object {
+      "-9m03zk|-ys9vce": 2,
+    }
+  `);
+  expect(serialize(scopeB)).toMatchInlineSnapshot(`
+    Object {
+      "-9m03zk|-ys9vce": 3,
+    }
+  `);
+});
