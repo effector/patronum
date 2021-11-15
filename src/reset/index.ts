c0044b3d7ff64a6df5ccdd81aae04e3287cb0b04
Sergey Sova <mail@sergeysova.com>
2021-11-15 17:54:37 +0300

    feat(reset): add tests and source

---
+++ src/reset/index.ts
@@ -0,0 +1,15 @@
+import { Unit, Store } from 'effector';
+
+export function reset({
+  clock,
+  target,
+}: {
+  clock: Unit<any> | Array<Unit<any>>;
+  target: Store<any> | Array<Store<any>>;
+}): void {
+  const targets = Array.isArray(target) ? target : [target];
+  const clocks = Array.isArray(clock) ? clock : [clock];
+  targets.forEach((target) => {
+    target.reset.call(target, clocks);
+  });
+}
