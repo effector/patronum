65395d82c489d7c31e0ca7339e75dc62f087f147
Sergey Sova <mail@sergeysova.com>
2022-05-11 15:46:27 +0400

    docs: upgrade theme

---
+++ website/src/pages/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Link from '@docusaurus/Link';
+
+export default function HomePage() {
+  const { siteConfig } = useDocusaurusContext();
+
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className="hero hero--primary">
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div>
+            <Link className="button button--secondary button--lg" to="/docs/intro">
+              Docusaurus Tutorial - 5min ⏱️
+            </Link>
+          </div>
+        </div>
+      </header>
+    </Layout>
+  );
+}
