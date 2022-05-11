f0fb132141414a6a61ab97cd91b08ccd1427866f
sergeysova <5620073+sergeysova@users.noreply.github.com>
2022-05-11 13:02:49 +0000

    Deploying to gh-pages from @ effector/patronum@60a4aa16348b7e30bba96289942b8859931aa995 🚀

---
+++ assets/js/8d0041db.0a343e00.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[885],{3769:function(e){e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
