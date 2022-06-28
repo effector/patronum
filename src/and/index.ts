6fff7e08e15cdf712c68e1af6908fc1dfd989ffc
Sergey Sova <mail@sergeysova.com>
2022-06-28 20:38:23 +0300

    fix(and,or): types when used different store types #206

--- src/and/index.ts
+++ src/and/index.ts
@@ -1,6 +1,6 @@
 import { combine, Store } from 'effector';
 
-export function and(...stores: Array<Store<unknown>>): Store<boolean> {
+export function and(...stores: Array<Store<any>>): Store<boolean> {
   return combine(stores, (values) =>
     values.reduce((all, current) => Boolean(all) && Boolean(current)),
   ) as Store<boolean>;
