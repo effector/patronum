eba0e5524d991c912658ebeeb977b8ca2cf491b7
Igor Kamyshev <igor@kamyshev.me>
2022-08-12 13:16:14 +0700

    chore(empty): fixes after review

--- src/empty/index.ts
+++ src/empty/index.ts
@@ -1,7 +1,5 @@
 import { Store } from 'effector';
 
-import { equals } from '../equals';
-
 export function empty<A>(a: Store<A | null>): Store<boolean> {
-  return source.map(value => value === null);
+  return a.map((value) => value === null);
 }
