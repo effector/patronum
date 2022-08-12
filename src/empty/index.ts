1637a96d13b4e93a94ad45410634dd1c0a7efd80
Sergey Sova <5620073+sergeysova@users.noreply.github.com>
2022-08-12 09:16:01 +0300

    Update src/empty/index.ts

--- src/empty/index.ts
+++ src/empty/index.ts
@@ -2,6 +2,6 @@ import { Store } from 'effector';
 
 import { equals } from '../equals';
 
-export function empty<A>(a: Store<A | null>): Store<boolean> {
+export function empty<A>(source: Store<A | null>): Store<boolean> {
   return source.map(value => value === null);
 }
