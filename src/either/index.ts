d9e5116dca151243bb6d916198de8ad08958183d
Sergey Sova <mail@sergeysova.com>
2022-08-25 12:51:01 +0400

    fix(either): improve typings

--- src/either/index.ts
+++ src/either/index.ts
@@ -3,8 +3,16 @@ import { combine, is, Store, Unit } from 'effector';
 
 interface EitherConfig<Then, Other> {
   filter: Store<boolean>;
-  then: Then | Store<Then>;
-  other: Other | Store<Other>;
+  then: Then extends Unit<any>
+    ? Then extends Store<any>
+      ? Then
+      : { error: 'only stores or generic values allowed' }
+    : Then;
+  other: Other extends Unit<any>
+    ? Other extends Store<any>
+      ? Other
+      : { error: 'only stores or generic values allowed' }
+    : Other;
 }
 
 export function either<Then, Other>(
@@ -19,16 +27,36 @@ export function either<Then, Other>(
       ? Other
       : { error: 'only stores or generic values allowed' }
     : Other,
-): Store<Then | Other>;
+): Then extends Store<infer T>
+  ? Other extends Store<infer O>
+    ? Store<T | O>
+    : Store<T | Other>
+  : Other extends Store<infer O>
+  ? Store<Then | O>
+  : Store<Then | Other>;
 
 export function either<Then, Other>(
   config: EitherConfig<Then, Other>,
-): Store<Then | Other>;
+): Then extends Store<infer T>
+  ? Other extends Store<infer O>
+    ? Store<T | O>
+    : Store<T | Other>
+  : Other extends Store<infer O>
+  ? Store<Then | O>
+  : Store<Then | Other>;
 
 export function either<Then, Other>(
   filterOptions: Store<boolean> | EitherConfig<Then, Other>,
-  then?: Then | Store<Then>,
-  other?: Other | Store<Other>,
+  then?: Then extends Unit<any>
+    ? Then extends Store<any>
+      ? Then
+      : { error: 'only stores or generic values allowed' }
+    : Then,
+  other?: Other extends Unit<any>
+    ? Other extends Store<any>
+      ? Other
+      : { error: 'only stores or generic values allowed' }
+    : Other,
 ): Store<Then | Other> {
   if (is.store(filterOptions)) {
     return combine(
@@ -38,5 +66,5 @@ export function either<Then, Other>(
       (filter, then, other) => (filter ? then : other),
     );
   }
-  return either(filterOptions as EitherConfig<Then, Other>);
+  return either(filterOptions as EitherConfig<Then, Other>) as Store<Then | Other>;
 }
--- test-typings/either.ts
+++ test-typings/either.ts
@@ -35,6 +35,12 @@ import { either } from '../src/either';
   either(createStore(null), $first, $second);
 }
 
+// Allows to receive only literals as branches
+{
+  const $filter = createStore(true);
+  expectType<Store<string | number>>(either($filter, 1, ''));
+}
+
 // Doens't allow to pass non-store as branches
 {
   const $filter = createStore(true);
@@ -50,4 +56,56 @@ import { either } from '../src/either';
 
   // @ts-expect-error
   either($filter, createEffect(), createDomain());
+}
+
+// With config returns always store with merged type
+{
+  const $filter = createStore(true);
+  const $first = createStore(0);
+  const $second = createStore('');
+
+  expectType<Store<number | string>>(
+    either({ filter: $filter, then: $first, other: $second }),
+  );
+}
+
+// With config doesn't allow non-boolean store as filter
+{
+  const $first = createStore(0);
+  const $second = createStore('');
+
+  // @ts-expect-error
+  either({ filter: createStore(0), then: $first, other: $second });
+
+  // @ts-expect-error
+  either({ filter: createStore(''), then: $first, other: $second });
+
+  // @ts-expect-error
+  either({ filter: createStore([]), then: $first, other: $second });
+
+  // @ts-expect-error
+  either({ filter: createStore(null), then: $first, other: $second });
+}
+
+// With config doens't allow to pass non-store as branches
+{
+  const $filter = createStore(true);
+
+  // @ts-expect-error
+  either({ filter: $filter, then: createEvent(), other: createStore('') });
+
+  // @ts-expect-error
+  either({ filter: $filter, then: createStore(''), other: createEvent() });
+
+  // @ts-expect-error
+  either({ filter: $filter, then: createEffect(), other: createEvent() });
+
+  // @ts-expect-error
+  either({ filter: $filter, then: createEffect(), other: createDomain() });
+}
+
+// With config allows to receive only literals as branches
+{
+  const $filter = createStore(true);
+  expectType<Store<string | number>>(either({ filter: $filter, then: 1, other: '' }));
 }
