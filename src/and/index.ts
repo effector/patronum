8df0e2211be402210a8a46073376ff3a5fec49c7
Victor <yumaa.verdin@gmail.com>
2022-08-29 11:08:54 +0300

    short-circuit evaluation in `and` and `or` operators

--- src/and/index.ts
+++ src/and/index.ts
@@ -1,10 +1,12 @@
 import { combine, Store } from 'effector';
 
 export function and(...stores: Array<Store<any>>): Store<boolean> {
-  return combine(stores, (values) =>
-    values.reduce(
-      (all, current) => Boolean(all) && Boolean(current),
-      Boolean(values[0]),
-    ),
-  ) as Store<boolean>;
+  return combine(stores, (values) => {
+    for (const value of values) {
+      if (!Boolean(value)) {
+        return false
+      }
+    }
+    return Boolean(values.length) // `false` if zero stores were given
+  }) as Store<boolean>;
 }
--- src/or/index.ts
+++ src/or/index.ts
@@ -1,10 +1,12 @@
 import { combine, Store } from 'effector';
 
 export function or(...stores: Array<Store<any>>): Store<boolean> {
-  return combine(stores, (values) =>
-    values.reduce(
-      (all, current) => Boolean(all) || Boolean(current),
-      Boolean(values[0]),
-    ),
-  ) as Store<boolean>;
+  return combine(stores, (values) => {
+    for (const value of values) {
+      if (Boolean(value)) {
+        return true
+      }
+    }
+    return false
+  }) as Store<boolean>;
 }
