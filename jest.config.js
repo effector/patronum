284f9c98e639c872ef5dcfc979383a773559d5a3
AlexandrHoroshih <xad71rus@gmail.com>
2023-01-05 17:13:05 +0700

    Do not bail on CI

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   // automock: false,
 
   // Stop running tests after `n` failures
-  // bail: 0,
+  bail: false,
 
   // Respect "browser" field in package.json when resolving modules
   // browser: false,
