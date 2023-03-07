fe78c6baf6575d72afb0dcec2d3279c2df38a825
Mikhail Kireev <29187880+kireevmp@users.noreply.github.com>
2023-03-07 20:19:38 +0100

    feat(time): add type tests

--- src/time/index.ts
+++ src/time/index.ts
@@ -2,6 +2,8 @@ import { createEffect, Effect, Event, forward, restore, Store } from 'effector';
 
 const defaultNow = <Time = number>() => Date.now() as unknown as Time;
 
+type NoInfer<T> = [T][T extends any ? 0 : never];
+
 export function time<Time = number>({
   clock,
   getNow,
@@ -9,7 +11,7 @@ export function time<Time = number>({
 }: {
   clock: Event<any> | Effect<any, any, any> | Store<any>;
   getNow?: () => Time;
-  initial?: Time;
+  initial?: NoInfer<Time>;
 }): Store<Time> {
   const timeReader = getNow ?? defaultNow;
   const readNowFx = createEffect<void, Time>(timeReader);
