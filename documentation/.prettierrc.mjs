176cf5ff708e1a3a77d90e96476eb08e12bd9a2c
Sergey Sova <mail@sergeysova.com>
2024-08-31 14:39:48 +0400

    style: reformat with prettier

--- documentation/.prettierrc.mjs
+++ documentation/.prettierrc.mjs
@@ -1,5 +1,10 @@
 /** @type {import("prettier").Config} */
 export default {
+  arrowParens: 'always',
+  printWidth: 120,
+  trailingComma: 'all',
+  singleQuote: true,
+  jsxSingleQuote: true,
   plugins: ['prettier-plugin-astro'],
   overrides: [
     {
